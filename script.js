e94a4ed1ee8178c12a0e6b2eeb549c70ca550a96
ShaikArsalan347 <ShaikArsalan347@gmail.com>
2024-10-03 23:17:09 +0530

    responsive

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 "use strict";
+const navBtn = document.querySelector(".btn-mobile-nav");
+const navList = document.querySelectorAll(".main-nav-list");
+const navLink = document.querySelectorAll(".main-nav-link");
 const opensBtns = document.querySelectorAll(".openBtn");
 const showAnswer = document.querySelectorAll(".answer-box");
 const overlay = document.querySelectorAll(".question-box");
+
 for (let i = 0; i < opensBtns.length; i++) {
   opensBtns[i].addEventListener("click", function () {
     showAnswer[i].classList.toggle("show");
   });
 }
+navBtn.addEventListener("click", function () {
+  navList.forEach((each) => {
+    each.classList.toggle("show");
+  });
+});
